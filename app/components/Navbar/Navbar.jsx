3773600c007f495ca738e7d9a2b2a37d54d56520
anuragunnikannan <anuragunnikannan7@gmail.com>
2024-11-09 21:19:37 +0530

    added menu for profile and logout

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { Avatar, Box, Typography } from '@mui/material'
+import { Avatar, Box, Menu, MenuItem, Typography } from '@mui/material'
 import React, { useContext, useEffect, useState } from 'react'
 import styles from "./Navbar.module.css"
 import { ModeContext } from '@/app/CustomThemeProvider'
@@ -7,21 +7,22 @@ import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import { api } from '@/utils/apiFile';
 import Cookies from 'js-cookie';
+import { AccountCircle, Logout } from '@mui/icons-material';
 
 const Navbar = () => {
 
     const { mode, setMode } = useContext(ModeContext);
     const [userData, setUserData] = useState({});
     const [displayAvatar, setDisplayAvatar] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
 
     const toggleTheme = () => {
         setMode(mode === "light" ? "dark" : "light");
     }
 
     useEffect(() => {
-        console.log("inside useeffect", Cookies.get("isLoggedIn"));
         if (Cookies.get("isLoggedIn")) {
-            console.log("inside if block")
             setDisplayAvatar(true);
             api.get(`${process.env.NEXT_PUBLIC_API_URL}/get-user-data`).then((res) => {
                 setUserData(res?.data);
@@ -30,25 +31,71 @@ const Navbar = () => {
 
     }, [])
 
-    return (
-        <Box className={styles.navbar}>
-            <Typography variant="h4" sx={{ fontWeight: "900", color: "white" }}>CoderRank</Typography>
+    const randomColor = (initials) => {
+        let magicNumber = (initials?.charCodeAt(0) + initials?.charCodeAt(1)) / 2
+        let hex = Math.floor(magicNumber * 0xFFFFFF);
+        let color = "#" + hex.toString(16);
 
-            <Box className={styles.right_container}>
-                <Box onClick={toggleTheme}>
-                    {mode === "light" ? <DarkModeIcon sx={{
-                        color: "white", "&:hover": {
-                            cursor: "pointer"
-                        }
-                    }} /> : <LightModeIcon sx={{ color: "white" }} />}
+        return color;
+    }
+
+    const stringAvatar = (name) => {
+        return {
+            sx: {
+                "&:hover": { cursor: "pointer" },
+                bgcolor: randomColor(`${name?.split(' ')[0][0]}${name?.split(' ')[1][0]}`),
+            },
+            children: `${name?.split(' ')[0][0]}${name?.split(' ')[1][0]}`,
+        };
+    }
+
+    return (
+        <>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={() => setAnchorEl(null)}
+                sx={{
+                    "& .MuiPaper-root": {
+                        backgroundColor: "background"
+                    }
+                }}
+            >
+                <MenuItem onClick={() => setAnchorEl(null)} sx={{ display: "flex", gap: "10px" }}>
+                    <AccountCircle />
+                    <Typography>Profile</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => setAnchorEl(null)} sx={{ display: "flex", gap: "10px" }}>
+                    <Logout />
+                    <Typography>Logout</Typography>
+                </MenuItem>
+            </Menu>
+
+            <Box className={styles.navbar}>
+                <Typography variant="h4" sx={{ fontWeight: "900", color: "white" }}>CoderRank</Typography>
+
+                <Box className={styles.right_container}>
+                    <Box onClick={toggleTheme}>
+                        {mode === "light" ? <DarkModeIcon sx={{
+                            color: "white", "&:hover": {
+                                cursor: "pointer"
+                            }
+                        }} /> : <LightModeIcon sx={{ color: "white" }} />}
+                    </Box>
+
+                    {displayAvatar ?
+                        <Avatar
+                            {...stringAvatar(userData?.user_name)}
+                            onClick={(e) => setAnchorEl(e.currentTarget)}
+                        />
+                        : null}
                 </Box>
 
-                {displayAvatar ?
-                    <Avatar sx={{ "&:hover": { cursor: "pointer" } }}>{userData?.user_name?.split(" ")[0][0] + userData?.user_name?.split(" ")[1][0]}</Avatar>
-                    : null}
             </Box>
 
-        </Box>
+        </>
+
     )
 }
 
